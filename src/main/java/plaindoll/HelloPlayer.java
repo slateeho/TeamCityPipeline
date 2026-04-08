00ec2140f345d65264b3a3ce66c91d08274ad7da
Aleksandr Lisogor <slateeho@gmail.com>
2026-04-08 13:04:49 +0300

    Welcomer fix

--- src/main/java/plaindoll/HelloPlayer.java
+++ src/main/java/plaindoll/HelloPlayer.java
@@ -1,9 +1,13 @@
 package plaindoll;
 
-public class HelloPlayer{
-	public static void main(String[] args) {
-		Welcomer welcomer = new Welcomer();
-		System.out.println(welcomer.sayWelcome());
-		System.out.println(welcomer.sayFarewell());
-	}
+public class HelloPlayer {
+    public static void main(String[] args) {
+        Welcomer welcomer = new Welcomer();
+
+        System.out.println(welcomer.sayWelcome());
+        System.out.println(welcomer.sayFarewell());
+        System.out.println(welcomer.sayNeedGold());
+        System.out.println(welcomer.saySome());
+        System.out.println(welcomer.sayHunter());
+    }
 }
